a30f8edc1d0424389f7568ee8a60bc73e6bf2a7c
ArakiTakaki <arakitakaki.work@gmail.com>
2023-06-02 01:35:01 +0900

    fix (全体共通): 🐛 MSWを１回だけ呼び出すように変更
    MSWのstartイベントを２回呼び出すとwarnが表示されてしまうため、訂正

--- src/middlewares/MSWMiddleware.tsx
+++ src/middlewares/MSWMiddleware.tsx
@@ -1,22 +1,28 @@
 import is from '@/utils/is';
 import { EmptyMiddleware } from './EmptyMiddleware';
 import { TMiddlewareComponent } from './interfaces';
-import { useQuery } from '@tanstack/react-query';
+import { useLayoutEffect, useState } from 'react';
 
 const fetchWorker = async () => {
   const res = await import('@/mocks/browser');
   await res.browserWorker.start({
     onUnhandledRequest: (mockedReq, print) => {
-    if (!/$\/api/.test(mockedReq.url.pathname)) return null;
-    print.warning();
-  }
+      if (!/$\/api/.test(mockedReq.url.pathname)) return null;
+      print.warning();
+    }
   });
   return true;
 }
 
 export const MSWMiddleware: TMiddlewareComponent = is.production ? EmptyMiddleware : (props) => {
-  const query = useQuery(['msw-worker'], fetchWorker);
-  if (query.isLoading) return null;
+  // HACK: react query の場合 client ごと更新されるため MSWのstart eventが複数呼ばれてしまうため更新
+  const [isLoading, setIsLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    fetchWorker().then(() => setIsLoading(false))
+  }, [])
+
+  if (isLoading) return null;
   return (
     <>{props.children}</>
   )
