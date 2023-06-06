74a50578e2a3bed6aca36b1137c93e69564728b4
ArakiTakaki <arakitakaki.work@gmail.com>
2023-06-06 13:55:49 +0900

    update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,32 +1,29 @@
-// const path = require('path');
-//
-// const codeExt = ['ts', 'tsx'];
-// const testExt = ['spec.ts', 'spec.tsx', 'test.tsx', 'test.ts'];
-// const styleExt = ['css', 'scss'];
-// const textExt = ['md'];
-// const allExts = [...codeExt, ...styleExt, ...textExt, ...testExt];
-//
-// const createExt = (code) => `*.{${code.join(',')}}`;
+const path = require('path');
+
+const codeExt = ['ts', 'tsx'];
+const testExt = ['spec.ts', 'spec.tsx', 'test.tsx', 'test.ts'];
+const styleExt = ['css', 'scss'];
+const textExt = ['md'];
+const allExts = [...codeExt, ...styleExt, ...textExt, ...testExt];
+
+const createExt = (code) => `*.{${code.join(',')}}`;
 
 module.exports = {
-  // [createExt(allExts)]: (absolutePaths) => {
-  //   const cwd = process.cwd();
-  //   const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
-  //   return [`textlint --fix ${relativePaths}`, `cspell -c ./.cspellrc.json ${relativePaths}`];
-  // },
-  // [createExt(codeExt)]: (absolutePaths) => {
-  //   const cwd = process.cwd();
-  //   const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
-  //   return [`prettier --write ${relativePaths}`, `yarn eslint ${relativePaths}`];
-  // },
-  // [createExt(testExt)]: (absolutePaths) => {
-  //   const cwd = process.cwd();
-  //   const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
-  //   return [`vitest --run ${relativePaths}`];
-  // },
-  // [createExt(styleExt)]: (absolutePaths) => {
-  //   const cwd = process.cwd();
-  //   const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
-  //   return [`stylelint ${relativePaths}`];
-  // },
+  [createExt(allExts)]: (absolutePaths) => {
+    const cwd = process.cwd();
+    const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
+    return [`textlint --fix ${relativePaths}`, `cspell -c ./.cspellrc.json ${relativePaths}`];
+  },
+
+  [createExt(codeExt)]: (absolutePaths) => {
+    const cwd = process.cwd();
+    const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
+    return [`prettier --write ${relativePaths}`, `yarn eslint ${relativePaths}`];
+  },
+
+  [createExt(styleExt)]: (absolutePaths) => {
+    const cwd = process.cwd();
+    const relativePaths = absolutePaths.map((file) => path.relative(cwd, file)).join(' ');
+    return [`stylelint ${relativePaths}`];
+  },
 };
