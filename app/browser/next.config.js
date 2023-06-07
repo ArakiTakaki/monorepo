2b2ee6328ab274fa9b8bdffaade1b0c2d11dcbf9
ArakiTakaki <arakitakaki.work@gmail.com>
2023-06-07 23:40:07 +0900

    update

--- app/browser/next.config.js
+++ app/browser/next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
+const { join } = require('path');
+
 const nextConfig = {
-    linaria: {
-        /**
-         * https://vercel.com/docs/build-step#caching
-         * で記載されているディレクトリにビルド結果をキャッシュさせる
-         */
-        cacheDirectory: './.next/cache/.linaria-cache',
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  experimental: {
+    // https://nextjs.org/docs/pages/api-reference/next-config-js/output#caveats
+    outputFileTracingRoot: join(__dirname, '../../'),
+  },
+  linaria: {
+    /**
+     * https://vercel.com/docs/build-step#caching
+     * で記載されているディレクトリにビルド結果をキャッシュさせる
+     */
+    cacheDirectory: './.next/cache/.linaria-cache',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
