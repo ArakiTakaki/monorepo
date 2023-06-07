771483619939e4895f619acac432096f2d805199
ArakiTakaki <arakitakaki.work@gmail.com>
2023-06-07 23:31:56 +0900

    cache周りの削除

--- app/browser/next.config.js
+++ app/browser/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    linaria: {
+        /**
+         * https://vercel.com/docs/build-step#caching
+         * で記載されているディレクトリにビルド結果をキャッシュさせる
+         */
+        cacheDirectory: './.next/cache/.linaria-cache',
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 module.exports = nextConfig;
