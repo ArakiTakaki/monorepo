5a2669f8149fcd57af7cfc8d87723aaa02f84708
ArakiTakaki <arakitakaki.work@gmail.com>
2023-06-07 18:08:17 +0900

    feat (core): viteにプラグインを適用

--- app/core/vite.config.ts
+++ app/core/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import ssr from 'vite-plugin-ssr/plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), ssr({ prerender: true }), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
